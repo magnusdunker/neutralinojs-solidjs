f87d6d15ed51276f4c61a09e7b467872fd5fcb03
Magnus Dunker <magnusdunker@gmail.com>
2023-05-11 21:17:07 +0200

    Init

---
+++ solid-src/src/App.tsx
@@ -0,0 +1,34 @@
+import type { Component } from 'solid-js';
+import { createSignal, onMount, For } from "solid-js";
+
+import logo from './logo.svg';
+import styles from './App.module.css';
+import { filesystem } from "@neutralinojs/lib"
+
+const App: Component = () => {
+  onMount(async () => {
+    var res = await filesystem.readDirectory('./');
+    console.log(res);
+    
+  });
+  return (
+    <div class={styles.App}>
+      <header class={styles.header}>
+        <img src={logo} class={styles.logo} alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          class={styles.link}
+          href="https://github.com/solidjs/solid"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn Solid
+        </a>
+      </header>
+    </div>
+  );
+};
+
+export default App;
